1fa426d1d0b429ebd0ea26e90920c499fe9a07e7
wbonna352 <wojciechbonna@gmail.com>
2023-12-03 10:40:43 +0100

    add Day01 scala part two

--- scala/src/main/scala/Day01.scala
+++ scala/src/main/scala/Day01.scala
@@ -1,5 +1,5 @@
 import scala.io.Source
-import scala.util.Using
+import scala.util.matching.Regex
 
 object Day01 {
 
@@ -9,19 +9,64 @@ object Day01 {
     List(first, last).mkString.toInt
   }
 
-  def dayOne(): Unit = {
-    val input: Iterator[String] = Source.fromFile("inputs/Day01.input").getLines()
-    val result: Int =
-      input
-      .map(calibrationValue)
-      .sum
+  def calibrationValueWithWords(input: String): Int = {
 
-    println(result)
+    val digits: Map[String, Int] = (1 to 9).map(n => (n.toString, n)).toMap
+    val words: Map[String, Int] = Map(
+      "one" -> 1,
+      "two" -> 2,
+      "three" -> 3,
+      "four" -> 4,
+      "five" -> 5,
+      "six" -> 6,
+      "seven" -> 7,
+      "eight" -> 8,
+      "nine" -> 9
+    )
+
+    val mapper: Map[String, Int] = digits ++ words
+
+    val matchPattern: Regex =
+      mapper
+      .keys
+      .mkString("|")
+      .r
+
+    val matches: Iterator[String] =
+      input
+        .tails
+        .map(matchPattern.findPrefixOf)
+        .filter(_.nonEmpty)
+        .map(_.get)
+
+    val first = matches.next()
+    var last = first
+    while (matches.hasNext) last = matches.next()
+
+    s"${mapper(first)}${mapper(last)}".toInt
+
   }
+
+  def partOne(): Int = {
+    val input: Iterator[String] = Source.fromFile("inputs/Day01.input").getLines()
+
+      input
+        .map(calibrationValue)
+        .sum
+  }
+
+  def partTwo(): Int = {
+    val input: Iterator[String] = Source.fromFile("inputs/Day01.input").getLines()
+
+      input
+        .map(calibrationValueWithWords)
+        .sum
+  }
+
 
   def main(args: Array[String]): Unit = {
-    dayOne()
+    println("Part one: " + partOne())
+    println("Part two: " + partTwo())
   }
-
 
 }
