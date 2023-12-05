a35b1f473219f023bd9bd3471729180bd0a2f7fc
wbonna352 <wojciechbonna@gmail.com>
2023-12-05 22:05:20 +0100

    add scala Day04 part2

--- scala/src/main/scala/Day04.scala
+++ scala/src/main/scala/Day04.scala
@@ -1,3 +1,4 @@
+import scala.annotation.tailrec
 import scala.io.Source
 import scala.util.matching.Regex
 import scala.math.pow
@@ -8,6 +9,10 @@ object Day04 {
     def points: Int = {
       val cards: Int = win.intersect(played).length
       pow(2, cards - 1).toInt
+    }
+    def copies: List[Int] = {
+      val cards: Int = win.intersect(played).length
+      (1 to cards).map(_ + id).toList
     }
   }
   object Card {
@@ -25,6 +30,21 @@ object Day04 {
     }
   }
 
+  def collectScratchcards(original: List[Card]): List[Card] = {
+    @tailrec
+    def runner(acc: List[Card], toCopy: List[Card]): List[Card] = {
+      val copies: List[Card] =
+        toCopy
+          .flatMap(_.copies)
+          .flatMap(id => original.filter(_.id == id))
+
+      if (copies.isEmpty) acc
+      else runner(acc ++ copies, copies)
+    }
+
+    runner(original, original)
+  }
+
   def part1(): Int = {
     val input = Source.fromFile("inputs/Day04.input").getLines()
 
@@ -33,7 +53,16 @@ object Day04 {
       .sum
   }
 
+  def part2(): Int = {
+    val input = Source.fromFile("inputs/Day04.input").getLines().toList
+    val originalCards: List[Card] = input.map(Card(_))
+    val scratchcards: List[Card] = collectScratchcards(originalCards)
+
+    scratchcards.length
+  }
+
   def main(args: Array[String]): Unit = {
     println("Part1: " + part1())
+    println("Part2: " + part2())
   }
 }
