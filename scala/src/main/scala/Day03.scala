731bd74ce384535ada5aa64f6ba9636dae1d6881
wbonna352 <wojciechbonna@gmail.com>
2023-12-05 17:56:39 +0100

    add scala Day03 part2

--- scala/src/main/scala/Day03.scala
+++ scala/src/main/scala/Day03.scala
@@ -2,15 +2,15 @@ import scala.io.Source
 
 object Day03 {
 
-  val symbols: Array[Char] =
-    """~!@#$^%&*()_-+={}[]|;:\"'<,>?/"""
-      .toCharArray
-
   def part1(): Int = {
     val input: Array[Array[Char]] = Source.fromFile("inputs/Day03.input")
       .getLines()
       .map(_.toCharArray)
       .toArray
+
+    val symbols: Array[Char] =
+      """~!@#$^%&*()_-+={}[]|;:\"'<,>?/"""
+        .toCharArray
 
     val lineLength: Int = input(0).length
     val inputFlat: Array[Char] = input.flatten
@@ -40,7 +40,52 @@ object Day03 {
 
   }
 
+  def part2(): Int = {
+    val symbol: Char = '*'
+
+    val input: Array[Array[Char]] = Source.fromFile("inputs/Day03.input")
+      .getLines()
+      .map(_.toCharArray)
+      .toArray
+
+    val lineLength: Int = input(0).length
+    val inputFlat: Array[Char] = input.flatten
+    val inputSize = inputFlat.length
+
+    val symbolPositions: Array[Int] = inputFlat
+      .zipWithIndex
+      .filter(_._1 == symbol)
+      .map(_._2)
+
+    val adjacentPositions: Array[(Int, Array[Int])] =
+      symbolPositions
+        .map { i =>
+          (i,
+          (0 until inputSize)
+            .filter(p =>
+              (((p / lineLength) - (i / lineLength)).abs <= 1)
+                && (((p % lineLength) - (i % lineLength)).abs <= 1)
+            ).toArray)
+        }
+
+    val values: Array[(Array[Int], Int)] =
+      "\\d+".r
+        .findAllMatchIn(inputFlat.mkString)
+        .map(m => ((m.start until m.end).toArray, m.matched.toInt))
+        .toArray
+
+    adjacentPositions
+      .map(symbol =>
+        (symbol, values.filter(_._1.intersect(symbol._2).nonEmpty)))
+      .filter(_._2.length == 2)
+      .map(symbol => symbol._2.map(_._2))
+      .map(_.product)
+      .sum
+  }
+
   def main(args: Array[String]): Unit = {
     println("Part1: " + part1())
+    println("Part2: " + part2())
+
   }
 }
