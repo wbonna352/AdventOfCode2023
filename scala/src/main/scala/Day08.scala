c1d45046e8dc342694771684da07d0de86bbdf42
wbonna352 <wojciechbonna@gmail.com>
2023-12-26 21:20:40 +0100

    add scala Day08 part2

--- scala/src/main/scala/Day08.scala
+++ scala/src/main/scala/Day08.scala
@@ -23,34 +23,59 @@ object Day08 {
     }
   }
 
-  def execute(startNode: Node, finalNode: Node, instructions: Array[Char], network: List[NetworkLine]): Int = {
+  def execute(startNode: Node, pred: Node => Boolean, instructions: Array[Char], network: List[NetworkLine]): Int = {
 
     @tailrec
-    def run(n: Int, currentNode: Node, currentInstructions: Array[Char]): Int = {
-      if (currentNode == finalNode) n
+    def run(n: Int, node: Node, currentInstructions: Array[Char]): Int = {
+      if (pred(node)) n
       else run(
         n+1,
-        findNextNode(currentNode, currentInstructions.head, network),
+        findNextNode(node, currentInstructions.head, network),
         currentInstructions.tail :+ currentInstructions.head
       )
     }
     run(0, startNode, instructions)
   }
 
-  def part1(input: List[String]): Int = {
+  def lcm(a: Long, b: Long): Long = a * b / gcd(a, b)
+
+  @tailrec
+  def gcd(a: Long, b: Long): Long =
+    if (b == 0) a
+    else gcd(b, a % b)
+
+  def multipleExecute(startNodes: List[Node], pred: Node => Boolean, instructions: Array[Char], network: List[NetworkLine]): Long = {
+
+    startNodes
+      .map(execute(_, pred, instructions, network).toLong)
+      .reduce(lcm)
+  }
+
+  def part1(instructions: Array[Char], network: List[NetworkLine]): Int = {
     val startNode: Node = "AAA"
-    val finalNode: Node = "ZZZ"
+    def pred(node: Node): Boolean = node == "ZZZ"
+
+    execute(startNode, pred, instructions, network)
+  }
+
+  def part2(instructions: Array[Char], network: List[NetworkLine]): Long = {
+    val startNodes: List[Node] = network
+      .filter(_.main.endsWith("A"))
+      .map(_.main)
+    def endCondition(node: Node): Boolean = node.endsWith("Z")
+
+    multipleExecute(startNodes, endCondition, instructions, network)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = Source.fromFile("inputs/Day08.input").getLines().toList
     val instructions: Array[Char] = input.head.toCharArray
     val network: List[NetworkLine] = input
       .drop(2)
       .map(stringToNetworkLine)
 
-    execute(startNode, finalNode, instructions, network)
-  }
-
-  def main(args: Array[String]): Unit = {
-    val input = Source.fromFile("inputs/Day08.input").getLines().toList
-    println("Part1: " + part1(input))
+    println("Part1: " + part1(instructions, network))
+    println("Part2: " + part2(instructions, network))
   }
 
 }
